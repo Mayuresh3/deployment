bcb1a74fe62fd0c50918bd8667d0f651ddf77618
Mayuresh Rajendra Yadav <69915267+Mayuresh3@users.noreply.github.com>
2022-04-14 20:48:31 +0530

    Update f1.py

--- f1.py
+++ f1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import sklearn
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
