050a858790d1a23b7f8d459b33209534e6ce958b
manasi1910 <78139067+manasi1910@users.noreply.github.com>
2022-04-22 20:33:13 +0530

    Update f1.py

--- f1.py
+++ f1.py
@@ -16,110 +16,113 @@ import json
 from urllib.request import urlopen
 import seaborn
 
-st.title("KNN")
-uploaded_file = st.file_uploader(label="Upload CSV file",type=['csv','xlsx'])
+a=st.selectbox('Pick one', ['KMean', 'KNN'])
 
-# Dataset cleaning
-@st.cache
-def cleaning(file):
-    if file is not None:
-        df = pd.read_csv(file)
-        category_cols = []
-        num_cols=[c for c in list(df.columns) if df[c].dtype == 'int64' or df[c].dtype == 'float64']
-        for i in num_cols:
-            if df[i].isnull().sum().all() == True:
-                df[i]=df[i].fillna(df[i].mean())
+if a=='KNN':
+    st.title("KNN")
+    uploaded_file = st.file_uploader(label="Upload CSV file",type=['csv','xlsx'])
 
-        threshold = 10
-        for each in df.columns:
-            if df[each].nunique() < threshold:
-                category_cols.append(each)
-        for each in category_cols:
-            df[each] = df[each].astype('category')
+    # Dataset cleaning
+    @st.cache
+    def cleaning(file):
+        if file is not None:
+            df = pd.read_csv(file)
+            category_cols = []
+            num_cols=[c for c in list(df.columns) if df[c].dtype == 'int64' or df[c].dtype == 'float64']
+            for i in num_cols:
+                if df[i].isnull().sum().all() == True:
+                    df[i]=df[i].fillna(df[i].mean())
 
-        for i in category_cols:
-            if df[i].isnull().sum().all()== True:
-                df[i]=df[i].fillna(df[i].mode()[0])
- 
-        # label_encoder object knows how to understand word labels.
-        label_encoder = preprocessing.LabelEncoder()
-        # Encode labels in column 'species'.
-        for i in category_cols:
-            df[i]= label_encoder.fit_transform(df[i])
+            threshold = 10
+            for each in df.columns:
+                if df[each].nunique() < threshold:
+                    category_cols.append(each)
+            for each in category_cols:
+                df[each] = df[each].astype('category')
 
-        for i in df.columns:
-            if df[i].isnull().sum().all()==True:
-                df = df.dropna()
+            for i in category_cols:
+                if df[i].isnull().sum().all()== True:
+                    df[i]=df[i].fillna(df[i].mode()[0])
 
-        return df
+            # label_encoder object knows how to understand word labels.
+            label_encoder = preprocessing.LabelEncoder()
+            # Encode labels in column 'species'.
+            for i in category_cols:
+                df[i]= label_encoder.fit_transform(df[i])
 
-if uploaded_file is not None:
-    file=cleaning(uploaded_file)
-    uploaded_file=file.to_csv('file1.csv')
-    data = pd.read_csv('file1.csv')
-    st.header("Dataset")
-    max_rows = len(data)
-    st.write("Total number of rows in this dataset is-", max_rows)
-    a1=st.number_input('Pick a number of rows', 0, max_rows)
-    if a1>=1:
-        st.dataframe(data.head(a1))
-        list1=[]  
-        data.drop(data.columns[data.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
-        for i in data.columns:
-            list1.append(i)
-        target=st.selectbox("What is the target column?",(list1))
-        x=data.loc[:, data.columns != target]
-        y=data[target]
-        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=42)
+            for i in df.columns:
+                if df[i].isnull().sum().all()==True:
+                    df = df.dropna()
 
-        nn=st.number_input('Pick a KNN number', 1, 1000)
-        weights=st.selectbox("What is the weight type",("uniform","distance"))
+            return df
 
-        if data[target].dtype=='float':
-            model = neighbors.KNeighborsRegressor(n_neighbors = nn)
-            model.fit(X_train, y_train)  #fit the model
-            y_pred=model.predict(X_test) #make prediction on test set
-            score = r2_score(y_test, y_pred)
+    if uploaded_file is not None:
+        file=cleaning(uploaded_file)
+        uploaded_file=file.to_csv('file1.csv')
+        data = pd.read_csv('file1.csv')
+        st.header("Dataset")
+        max_rows = len(data)
+        st.write("Total number of rows in this dataset is-", max_rows)
+        a1=st.number_input('Pick a number of rows', 0, max_rows)
+        if a1>=1:
+            st.dataframe(data.head(a1))
+            list1=[]  
+            data.drop(data.columns[data.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+            for i in data.columns:
+                list1.append(i)
+            target=st.selectbox("What is the target column?",(list1))
+            x=data.loc[:, data.columns != target]
+            y=data[target]
+            X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=42)
+
+            nn=st.number_input('Pick a KNN number', 1, 1000)
+            weights=st.selectbox("What is the weight type",("uniform","distance"))
+
+            if data[target].dtype=='float':
+                model = neighbors.KNeighborsRegressor(n_neighbors = nn)
+                model.fit(X_train, y_train)  #fit the model
+                y_pred=model.predict(X_test) #make prediction on test set
+                score = r2_score(y_test, y_pred)
 
 
-        else:
-            knn = KNeighborsClassifier(n_neighbors=nn,weights=weights)
-            knn.fit(X_train,y_train)
-            y_pred=knn.predict(X_test)
-
-        check1 = st.checkbox("Accuracy Score")
-        if check1:
-            if data[target].dtype=='float64':
-                st.header("Accuracy Score")
-                st.subheader(score)
             else:
-                st.header("Accuracy Score")
-                st.subheader(metrics.accuracy_score(y_test,y_pred))
+                knn = KNeighborsClassifier(n_neighbors=nn,weights=weights)
+                knn.fit(X_train,y_train)
+                y_pred=knn.predict(X_test)
 
-        check2 = st.checkbox("HeatMap")
-        if check2:
-            fig, ax = plt.subplots()
-            sns.heatmap(data.corr(), ax=ax)
-            st.header("Heatmap")
-            st.write(fig)
-        
-        check3 = st.checkbox("KNN-Visualization")
-        if check3:
-            knn = KNeighborsClassifier(n_neighbors=nn,weights=weights)
-            column1=st.selectbox("What is the  column1 to be used for visualization?",(list1))
-            column2=st.selectbox("What is the  column2 to be used for visualization?",(list1))
-            if column1!=column2:
-                x = data[[column1,column2]].values
-                y1=data[target]
-                
-                y = data[target].astype(int).values
-                
-                knn.fit(x,y)
-                fig=plt.figure(figsize=(10, 5))
-                plot_decision_regions(x, y, clf=knn, legend=2)
-                plt.xlabel(column1)
-                plt.ylabel(column2)
-                st.pyplot(fig)
+            check1 = st.checkbox("Accuracy Score")
+            if check1:
+                if data[target].dtype=='float64':
+                    st.header("Accuracy Score")
+                    st.subheader(score)
+                else:
+                    st.header("Accuracy Score")
+                    st.subheader(metrics.accuracy_score(y_test,y_pred))
+
+            check2 = st.checkbox("HeatMap")
+            if check2:
+                fig, ax = plt.subplots()
+                sns.heatmap(data.corr(), ax=ax)
+                st.header("Heatmap")
+                st.write(fig)
+
+            check3 = st.checkbox("KNN-Visualization")
+            if check3:
+                knn = KNeighborsClassifier(n_neighbors=nn,weights=weights)
+                column1=st.selectbox("What is the  column1 to be used for visualization?",(list1))
+                column2=st.selectbox("What is the  column2 to be used for visualization?",(list1))
+                if column1!=column2:
+                    x = data[[column1,column2]].values
+                    y1=data[target]
+
+                    y = data[target].astype(int).values
+
+                    knn.fit(x,y)
+                    fig=plt.figure(figsize=(10, 5))
+                    plot_decision_regions(x, y, clf=knn, legend=2)
+                    plt.xlabel(column1)
+                    plt.ylabel(column2)
+                    st.pyplot(fig)
 
 if a=='KMean':
 
